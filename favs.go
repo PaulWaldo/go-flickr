a9321d5b190664a1cf87bd4ddca9e67ddcbaed00
Paul Waldo <paul.w.waldo@gmail.com>
2021-09-18 17:42:53 -0400

    Adds RandomFav

--- favs.go
+++ favs.go
@@ -1,61 +1,57 @@
 package flickr
 
+import (
+	"math/rand"
+	"strconv"
+)
+
 type Fav struct {
-	Id string
-	Title string
-	Owner string
-	FavedBy string
+	Id        string
+	Title     string
+	Owner     string
+	FavedBy   string
 	DateFaved string
-	Farm int
-	Secret string
-	Server string
+	Farm      int
+	Secret    string
+	Server    string
+}
+
+type RawFavedPhoto struct {
+	Date_Faved string
+	Farm       int
+	Id         string
+	IsFamily   int
+	IsFriend   int
+	IsPublic   int
+	Owner      string
+	Secret     string
+	Server     string
+	Title      string
 }
 
 type FavsRaw struct {
 	Photos struct {
-		Page int
-		Pages int
+		Page    int
+		Pages   int
 		PerPage int
-		Total int
-		Photo []struct {
-			Date_Faved string
-			Farm int
-			Id string
-			IsFamily int
-			IsFriend int
-			IsPublic int
-			Owner string
-			Secret string
-			Server string
-			Title string
-		}
+		Total   int
+		Photo   []RawFavedPhoto
 	}
 	Stat string
 }
 
 type FavsRaw2 struct {
 	Photos struct {
-		Page int
-		Pages int
+		Page    int
+		Pages   int
 		PerPage int
-		Photo []struct {
-			Date_Faved string
-			Farm int
-			Id string
-			IsFamily int
-			IsFriend int
-			IsPublic int
-			Owner string
-			Secret string
-			Server string
-			Title string
-		}
+		Photo   []RawFavedPhoto 
 		Total string
 	}
 	Stat string
 }
 
-func (client *Client) Favs (userId string) ([]Fav, error) {
+func (client *Client) Favs(userId string) ([]Fav, error) {
 	response, err := client.Request("favorites.getPublicList", Params{
 		"user_id": userId,
 	})
@@ -79,16 +75,70 @@ func (client *Client) Favs (userId string) ([]Fav, error) {
 
 	for _, photo := range raw.Photos.Photo {
 		favs = append(favs, Fav{
-			Id: photo.Id,
-			Title: photo.Title,
-			Owner: photo.Owner,
-			FavedBy: userId,
+			Id:        photo.Id,
+			Title:     photo.Title,
+			Owner:     photo.Owner,
+			FavedBy:   userId,
 			DateFaved: photo.Date_Faved,
-			Farm: photo.Farm,
-			Secret: photo.Secret,
-			Server: photo.Server,
+			Farm:      photo.Farm,
+			Secret:    photo.Secret,
+			Server:    photo.Server,
 		})
 	}
 
 	return favs, nil
+}
+
+func DivMod(dvdn, dvsr int) (q, r int) {
+	r = dvdn
+	for r >= dvsr {
+		q += 1
+		r = r - dvsr
+	}
+	return
+}
+
+func (client *Client) RandomFav(userId string) (RawFavedPhoto, error) {
+	const pageSize = 100
+	response, err := client.Request("favorites.getPublicList", Params{
+		"user_id": userId, "per_page": strconv.Itoa(pageSize),
+	})
+	if err != nil {
+		return RawFavedPhoto{}, err
+	}
+
+	raw := &FavsRaw{}
+	err = Parse(response, raw)
+
+	if err != nil {
+		raw := &FavsRaw2{}
+		err = Parse(response, raw)
+	}
+
+	if err != nil {
+		return RawFavedPhoto{}, err
+	}
+
+	photoNum := rand.Intn(raw.Photos.Total)
+	page, offset := DivMod(photoNum, pageSize)
+	page += 1 // Account for API pages starting at 1
+	response, err = client.Request("favorites.getPublicList", Params{
+		"user_id": userId, "per_page": strconv.Itoa(pageSize), "page": strconv.Itoa(page),
+	})
+	if err != nil {
+		return RawFavedPhoto{}, err
+	}
+
+	raw = &FavsRaw{}
+	err = Parse(response, raw)
+
+	if err != nil {
+		raw := &FavsRaw2{}
+		err = Parse(response, raw)
+	}
+
+	if err != nil {
+		return RawFavedPhoto{}, err
+	}
+	return raw.Photos.Photo[offset], nil
 }
